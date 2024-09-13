a4040945c161257317664edd06a70b7e034943ba
Juan <146742892+GitNinja0@users.noreply.github.com>
2024-09-13 18:48:35 +0200

    First commit and create proyect

---
+++ src/main/java/com/decagame/decagame_backend/DecagameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.decagame.decagame_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecagameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecagameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decagame/decagame_backend/DecagameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decagame.decagame_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecagameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
